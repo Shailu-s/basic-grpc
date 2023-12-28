727ad72194e0d80a23a20a7b48ef5a3df5cc3790
Shailendra Singh <emailid462@gmail.com>
2023-12-28 17:03:42 +0530

    added client and server code

--- cmd/server.go
+++ cmd/server.go
@@ -1,40 +1,115 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	"strings"
 
+	pb "github.com/shailu-s/basic-grpc/pkg/gopher"
 	"github.com/spf13/cobra"
+	"golang.org/x/xerrors"
+	"google.golang.org/grpc"
 )
 
+const (
+	port         = ":9000"
+	KuteGoAPIURL = "https://kutego-api-xxxxx-ew.a.run.app"
+)
+
+// server is used to implement gopher.GopherServer.
+type Server struct {
+	pb.UnimplementedGopherServer
+}
+
+type Gopher struct {
+	URL string `json: "url"`
+}
+
+// serverCmd represents the server command
 // serverCmd represents the server command
 var serverCmd = &cobra.Command{
 	Use:   "server",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Starts the Schema gRPC server",
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("server called")
+		lis, err := net.Listen("tcp", port)
+		if err != nil {
+			log.Fatalf("failed to listen: %v", err)
+		}
+
+		grpcServer := grpc.NewServer()
+
+		// Register services
+		pb.RegisterGopherServer(grpcServer, &Server{})
+
+		log.Printf("GRPC server listening on %v", lis.Addr())
+
+		if err := grpcServer.Serve(lis); err != nil {
+			log.Fatalf("failed to serve: %v", err)
+		}
 	},
+}
+
+// GetGopher implements gopher.GopherServer
+func (s *Server) GetGopher(ctx context.Context, req *pb.GopherRequest) (*pb.GopherReply, error) {
+	res := &pb.GopherReply{}
+
+	// Check request
+	if req == nil {
+		fmt.Println("request must not be nil")
+		return res, xerrors.Errorf("request must not be nil")
+	}
+
+	if req.Name == "" {
+		fmt.Println("name must not be empty in the request")
+		return res, xerrors.Errorf("name must not be empty in the request")
+	}
+
+	log.Printf("Received: %v", req.GetName())
+
+	// Call KuteGo API in order to get Gopher's URL
+	response, err := http.Get(KuteGoAPIURL + "/gophers?name=" + req.GetName())
+	if err != nil {
+		log.Fatalf("failed to call KuteGoAPI: %v", err)
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode == 200 {
+		// Transform our response to a []byte
+		body, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			log.Fatalf("failed to read response body: %v", err)
+		}
+
+		// Put only needed informations of the JSON document in our array of Gopher
+		var data []Gopher
+		err = json.Unmarshal(body, &data)
+		if err != nil {
+			log.Fatalf("failed to unmarshal JSON: %v", err)
+		}
+
+		// Create a string with all of the Gopher's name and a blank line as separator
+		var gophers strings.Builder
+		for _, gopher := range data {
+			gophers.WriteString(gopher.URL + "\n")
+		}
+
+		res.Message = gophers.String()
+	} else {
+		log.Fatal("Can't get the Gopher :-(")
+	}
+
+	return res, nil
 }
 
 func init() {
 	rootCmd.AddCommand(serverCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// serverCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// serverCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.21.0
 
 require (
 	github.com/spf13/cobra v1.8.0
+	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543
 	google.golang.org/grpc v1.60.1
 	google.golang.org/protobuf v1.32.0
 )
